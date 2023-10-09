4e6b0072b6e248044742797871e7f4797c8fa8cc
JesseIguess <blueberry21007@gmail.com>
2023-10-09 02:31:24 +0000

    heheheheheheh

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
 let colors = ["#fcba03", "#ff3d3d", "#443dff", "#2fbd61", "#f2981b", "#8ea630", "#a63061"];
+let hoveredCountry = "";
+let tooltip;
 
+window.onerror = (event, source, lineno, colno, error) => {
+    alert(event);
+}
 
 function colorFromString(str) {
-    return colorFromIndex(str.charCodeAt(0) + str.charCodeAt(colors.length - 1));
+    let index = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        index += str.charCodeAt(i);
+    }
+
+    return colorFromIndex(index);
 }
 function colorFromIndex(index) {
     return colors[index % colors.length];
 }
 
 function setColor(element, color) {
-    element.style.stroke = "#333";
     element.style.fill = color;
 }
 
 function loadMap() {
-    fetch("BlankMap-World-Sovereign_Nations.svg").then(async function(res) {
+    fetch("world.svg").then(async function(res) {
         document.getElementById("map").innerHTML = await res.text();
 
-        document.querySelectorAll("path").forEach((element) => {
-            const name = element.querySelector("title");
-            if (!name) return;
-            const color = colorFromString(name.innerHTML);
-
-            setColor(element, color);
-        });
-
-        document.querySelectorAll("g").forEach((element) => {
-            const name = element.querySelector("title");
-            if (!name) return;
-            const color = colorFromString(name.innerHTML);
-
-            setColor(element, color);
-
-            element.querySelectorAll("path").forEach((element2) => {
-                setColor(element2, color);
+        document.querySelectorAll("#map path").forEach((element) => {
+            // let name = element.getAttribute("name") || element.classList[0];
+            // let name = element.classList[0] || element.getAttribute("name");
+            let className = "";
+            element.classList.forEach((kys) => {
+                className += `${kys} `;
             });
+            let name = element.getAttribute("name") || className;
+            if (name == className) {
+                element.setAttribute("name", name);
+            }
+            element.onmouseover = () => {tooltip.innerHTML = name;};
+            element.onmouseleave = () => {tooltip.innerHTML = "Hover over a country";};
+            element.classList.add("country");
+
+            // alert(`lust: ${element.classList[0]}\nname: ${element.getAttribute('name')}\nactual name: ${name}`);
+            // throw new Error("killys");
+            // alert(name);
+
+            const color = colorFromString(name);
+
+            setColor(element, color);
         });
+    }).catch((event) => {
+        alert(event);
     });
 }
